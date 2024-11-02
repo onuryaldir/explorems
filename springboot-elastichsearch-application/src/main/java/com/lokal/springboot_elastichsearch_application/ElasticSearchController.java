02e27c1b2b74e63e7eb2a39819b0471a1d271666
Onur <onrdy96@gmail.com>
2024-11-02 18:09:59 +0300

    renaming

--- springboot-elastichsearch-application/src/main/java/com/lokal/springboot_elastichsearch_application/ElasticSearchController.java
+++ springboot-elastichsearch-application/src/main/java/com/lokal/springboot_elastichsearch_application/ElasticSearchController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.util.List;
 
-@RestController
+@RestController("api/explore")
 public class ElasticSearchController {
 
     @Autowired
